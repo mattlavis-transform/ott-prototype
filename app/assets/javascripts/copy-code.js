57af48e2ac44ca717d70b9d087c633150e05a583
Matt Lavis <matthew.lavis@gmail.com>
2021-05-12 14:24:25 +0100

    Update to copy code

--- app/assets/javascripts/copy-code.js
+++ app/assets/javascripts/copy-code.js
@@ -1,13 +1,12 @@
 $(function () {
     $("#copy_comm_code").click(function (event) {
         copyToClipboard("4103900000");
-        // $(this).text("Commodity code copied");
-        // $(this).removeAttr("href");
-        // $(this).removeClass("pseudo-link");
-        // $(this).fadeOut(750)
-        $(".copied").css("display", "block");
-        //$(".copied").show();
-        $(".copied").delay(500).fadeOut(900);
+
+        $(".copied").css("text-indent", "0");
+        $(".copied").delay(500).fadeOut(750, function() {
+            $(".copied").css("text-indent", "-999em");
+            $(".copied").css("display", "block");
+        });
         event.preventDefault();
     });
 
